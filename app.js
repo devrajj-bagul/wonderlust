3df1cc2b329f910a82b031de12a062cf91ff1201
Devraj Bagul <devrajbagul858@gmail.com>
2024-07-12 18:39:30 +0530

    phase 1 module 3

--- app.js
+++ app.js
@@ -1,2 +1,39 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const app = express();
+const Listing = require ("./models/listing.js");
+
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main().then(() => {
+    console.log("connected To DB");
+}).catch((err) => {
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+app.get("/", (req, res) => {
+    res.send("hi , Iam Root ");
+});
+
+app.get("/testListing", async (req, res) =>{
+    let sampleListing = new Listing({
+        title: "My Home",
+        description: " One room And Keychain for prepare food",
+        price: 3000,
+        location: "Malegaon , Nashik , Maharashtra ",
+        country: "India",
+    });
+
+    await sampleListing.save()
+    console.log(" Sample Was Saved ");
+    res.send("Secessfull Testing");
+});
+
+app.listen(8080, () => {
+    console.log("server Is Listning To port 8080")
+});
---
+++ models/listing.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema ({
+    title:{
+        type:String,
+        required: true,
+    },
+    description: String,
+
+    image: {
+        type: String,
+        set: (v) => v=== "" ? "https://unsplash.com/photos/green-palm-tree-near-silver-sedan-02fRawxKwbA" : v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
+
