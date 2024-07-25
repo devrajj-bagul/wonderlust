ff5b5f528778fd99734a1ab080945175a38a689a
Devraj Bagul <devrajbagul858@gmail.com>
2024-07-26 00:22:54 +0530

    Authorization For Reviews Fun Added

--- routes/listing.js
+++ routes/listing.js
@@ -20,7 +20,13 @@ router.get("/new", isLoggedIn, (req, res) => {
 // Show Route 
 router.get("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews").populate("owner", "username");
+    const listing = await Listing.findById(id).populate({
+        path:"reviews", 
+        populate:{
+        path: "author",
+    },
+})
+.populate("owner", "username");
     if(!listing){
         req.flash("error", "Listing you requested for does not exist!");
         return res.redirect("/listings");
