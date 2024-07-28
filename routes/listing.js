8ad0124eed446b1d229f6c3a5405a7b87876f8cc
Devraj Bagul <devrajbagul858@gmail.com>
2024-07-28 23:02:24 +0530

    Add Router.route & Image Upload and use Multer Labrary

--- routes/listing.js
+++ routes/listing.js
@@ -5,30 +5,31 @@ const Listing = require("../models/listing.js");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
   
 const listingController = require("../controllers/listing.js");
+const multer  = require("multer");
+const upload = multer({ dest: "uploads/" });
 
-// Index route 
-router.get("/", wrapAsync(listingController.index)); 
+// Index route   //Create ROute 
+router.route("/")
+.get( wrapAsync(listingController.index))
+// .post( isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+.post( upload.single("listing[image]"), (req, res) => {
+    res.send(req.file);
+});
+
 
 // New ROute 
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
-// Show Route 
-router.get("/:id", wrapAsync( listingController.showListing));
 
-
-//Create ROute 
-router.post("/", isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+// Show Route   //Update ROute   // Delete Route
+router.route("/:id")
+.get( wrapAsync( listingController.showListing))
+.put( isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing))
+.delete( isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
 
 //Edit Route 
 router.get("/:id/edit", isLoggedIn , isOwner, wrapAsync(listingController.renderEditForm)
 );
-
-//Update ROute 
-router.put("/:id", isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing));
-
-// Delete Route
-router.delete("/:id", isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
-
 
 module.exports = router;
